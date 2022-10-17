37047abbc2bfd40021ab5471cfa72857f61bc2ca
ttom-eragiapreso <109075301+ttom-eragiapreso@users.noreply.github.com>
2022-10-17 15:03:42 +0200

    upload photos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
 console.log("Slider app Loaded")
+
+const arrowUp = document.querySelector(".arrow-up")
+const arrowDown = document.querySelector(".arrow-down")
+const photoContainer = document.querySelector(".photo-container")
+const aside = document.querySelector(".aside")
+
+let imagesTags = ``;
+
+
+
+
+
+
+
+arrowUp.addEventListener("click", function(){
+  console.log("cliccato")
+})
